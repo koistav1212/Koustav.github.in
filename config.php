9652cb40e6b74515b1aebe2a396ea5804a72a75a
#KOUSTAV <58260635+koistav1212@users.noreply.github.com>
2020-05-23 19:56:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'test');
+define('DB_SERVER', 'sql9.freemysqlhosting.net ');
+define('DB_USERNAME', 'sql9342795');
+define('DB_PASSWORD', 'RrcdYEhH51');
+define('DB_NAME', 'sql9342795');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
